3d44584bbe12dda3e21a80541a7b0158e42b2c62
Vadym4ik33 <98187784+Vadym4ik33@users.noreply.github.com>
2026-06-08 16:45:26 +0300

    Add files via upload

---
+++ bubble_sort.pas
@@ -0,0 +1,31 @@
+﻿program bubblesort;
+var
+  a: array[1..8] of integer;
+  tmp: integer;
+begin
+  Randomize;
+  for var k :=1 to 8 do
+  begin
+    a[k] := random(100);
+    write(a[k], ' ');
+  end;
+  
+  writeln;
+  
+  for var j:= 1 to 7 do
+  begin
+    for var i:= 1 to 7 do
+    begin
+      if a[i] > a[i+1] then
+      begin
+        tmp := a[i];
+        a[i] := a[i+1];
+        a[i+1] := tmp;
+      end;
+    end;
+  end;
+  
+  writeln('массив после сортировки');
+  for var i:= 1 to 8 do
+    write(a[i], ' ');
+end.
---
+++ linear_search.pas
@@ -0,0 +1,31 @@
+﻿program Linear_Search;
+var
+  a: array[1..8] of integer;
+  searchNumber: integer; 
+  foundIndex: integer; 
+begin
+
+  a[1] := 10; a[2] := 25; a[3] := 32; a[4] := 45;
+  a[5] := 55; a[6] := 67; a[7] := 78; a[8] := 99;
+
+
+  writeln('Доступные числа в массиве:');
+  for var i := 1 to 8 do
+    write(a[i], ' ');
+  writeln; 
+
+  write('Какое число из массива ты хочешь найти? ');
+  readln(searchNumber);
+
+  foundIndex := 0; 
+  for var i:= 1 to 8 do
+  begin
+    if searchNumber = a[i] then 
+      foundIndex := i
+  end;
+
+  if foundIndex > 0 then
+    writeln('Число ', searchNumber, ' найдено в ячейке под номером: ', foundIndex)
+  else
+    writeln('Увы, числа ', searchNumber, ' нет в этом массиве.');
+end.
